crdroidandroid/android_packages_services_OmniJaws

OmniJaws: Use client from system
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

app/src/main/java/org/omnirom/omnijaws/DetailedWeatherView.java
@@ -28,7 +28,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.crdroid.OmniJawsClient;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;

app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
@@ -30,7 +30,7 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.crdroid.OmniJawsClient;
 import org.omnirom.omnijaws.widget.WeatherAppWidgetProvider;
 
 import java.util.ArrayList;

app/src/main/java/org/omnirom/omnijaws/WeatherActivity.java
@@ -23,7 +23,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.crdroid.OmniJawsClient;
 
 import java.util.Calendar;
 

app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
@@ -16,7 +16,7 @@
 package org.omnirom.omnijaws;
 
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.crdroid.OmniJawsClient;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;

app/src/main/java/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.java
@@ -51,7 +51,7 @@ import org.omnirom.omnijaws.Config;
 import org.omnirom.omnijaws.R;
 import org.omnirom.omnijaws.SettingsActivity;
 import org.omnirom.omnijaws.WeatherActivity;
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.crdroid.OmniJawsClient;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;

ApolloOS/packages_services_OmniJaws

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

app/src/main/java/org/omnirom/omnijaws/DetailedWeatherView.java
@@ -28,7 +28,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.apollo.OmniJawsClient;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;

app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
@@ -30,9 +30,10 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.apollo.OmniJawsClient;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.omnirom.omnijaws.client.OmniJawsClient;
 import org.omnirom.omnijaws.widget.WeatherAppWidgetProvider;
 
 import java.util.ArrayList;

app/src/main/java/org/omnirom/omnijaws/WeatherActivity.java
@@ -22,7 +22,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import org.omnirom.omnijaws.client.OmniJawsClient;
+import com.android.internal.util.apollo.OmniJawsClient;
 
 import java.util.Calendar;
 

app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
@@ -15,8 +15,9 @@
  */
 package org.omnirom.omnijaws;
 
+import com.android.internal.util.apollo.OmniJawsClient;
+
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
-import org.omnirom.omnijaws.client.OmniJawsClient;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;

app/src/main/java/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.java
@@ -37,11 +37,12 @@ import android.util.SizeF;
 import android.view.View;
 import android.widget.RemoteViews;
 
+import com.android.internal.util.apollo.OmniJawsClient;
+
 import org.omnirom.omnijaws.Config;
 import org.omnirom.omnijaws.R;
 import org.omnirom.omnijaws.SettingsActivity;
 import org.omnirom.omnijaws.WeatherActivity;
-import org.omnirom.omnijaws.client.OmniJawsClient;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;